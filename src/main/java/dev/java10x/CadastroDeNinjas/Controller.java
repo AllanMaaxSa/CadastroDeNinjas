c166ec4b3d380c1134d745301e8ad060eac0dac7
Allan Max <allanmaaxsa.ti@gmail.com>
2026-03-25 00:04:20 -0300

    Revert "Cria uma entidade no banco de dados apartir do NinjaModel Aula[3]"

--- src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 
-public class NinjaController {
+public class Controller {
 
     @GetMapping("/boasVindas")
     public String boasVindas(){
