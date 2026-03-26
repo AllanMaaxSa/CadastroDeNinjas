cebfd14936e0049e017c64e8d8867f8efde1ab58
Allan Max <allanmaaxsa.ti@gmail.com>
2026-03-26 00:13:42 -0300

    feat: Colocao o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -4,8 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
-
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //TODO: JPA = Java Persistence API,
 
@@ -14,7 +15,9 @@ import jakarta.persistence.GeneratedValue;
 
 //TODO: tb = vem table - tabela
 @Table(name = "tb_cadastro")
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -33,37 +36,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")// Foreing Key ou chave estrangeira
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
